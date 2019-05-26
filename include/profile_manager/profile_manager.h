99b3be687c4059fd378be18a6ed42363468a1290
Atthapron Phinitnart <qwertygms@hotmail.com>
2019-05-27 02:07:19 +0700

    add profile_manager class

---
+++ include/profile_manager/profile_manager.h
@@ -0,0 +1,28 @@
+#ifndef PROFILE_MANAGER_H
+#define PROFILE_MANAGER_H
+
+#include <ros/ros.h>
+
+#include <boost/thread.hpp>
+#include <profile_manager/config.h>
+
+class ProfileManager{
+    protected:
+        bool starting;
+        bool isReady;
+
+        boost::thread worker;
+        boost::mutex mutex;
+        boost::condition_variable cv;
+
+    public:
+        ProfileManager(ros::NodeHandle &n);
+
+    public:
+        virtual void run() = 0;
+        bool start();
+        bool stop();
+    
+};
+
+#endif
---
+++ src/profile_manager/profile_manager.cpp
@@ -0,0 +1,13 @@
+#include <profile_manager/profile_manager.h>
+
+ProfileManager::ProfileManager(ros::NodeHandle &n){
+    //implement here
+}
+
+bool ProfileManager::start(){
+    //implement here
+}
+
+bool ProfileManager::stop(){
+    //implement here
+}
