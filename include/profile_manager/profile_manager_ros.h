41c5166e07da3d878e2f742905ff73226da7d1ef
Atthapron Phinitnart <qwertygms@hotmail.com>
2019-05-27 02:14:41 +0700

    add profile_manager_ros class

---
+++ include/profile_manager/profile_manager_ros.h
@@ -0,0 +1,35 @@
+#ifndef PROFILE_MANAGER_ROS_H
+#define PROFILE_MANAGER_ROS_H
+
+#include <ros/ros.h>
+#include <profile_manager/profile_manager.h>
+#include <skeleton_msgs/People.h>
+#include <dinsow_msgs/DinsowAction.h>
+#include <geometry_msgs/Point.h>
+#include <dinsow_msgs/DinsowStore.h>
+
+class ProfileManagerRos : public ProfileManager{
+    private:
+        ros::NodeHandle *n;
+        ros::Publisher action_pub;
+        ros::Publisher angle_pub;
+        ros::Publisher state_pub;
+        ros::Publisher focus_point_pub;
+        ros::ServiceServer start_service;
+        ros::ServiceServer stop_service;
+        ros::ServiceClient dinsow_config_client;
+    public:
+        ProfileManagerRos(ros::NodeHandle &n);
+
+        void peopleCb(const skeleton_msgs::People::ConstPtr &p);
+        
+        void publish(const dinsow_msgs::DinsowAction &action);
+        void publish(const std::string &s);
+        void publish(const geometry_msgs::Point &p);
+        
+        void run();
+    private:
+        void callDinsowConfig();
+};
+
+#endif;
---
+++ src/profile_manager/profile_manager_ros.cpp
@@ -0,0 +1,29 @@
+#include <profile_manager/profile_manager_ros.h>
+
+ProfileManagerRos::ProfileManagerRos(ros::NodeHandle &n){
+    //implement here
+}
+
+void ProfileManagerRos::peopleCb(const skeleton_msgs::People::ConstPtr &p){
+    //implement here
+}   
+
+void ProfileManagerRos::publish(const dinsow_msgs::DinsowAction &action){
+    //implement here
+}
+
+void ProfileManagerRos::publish(const std::string &s){
+    //implement here
+}
+
+void ProfileManagerRos::publish(const geometry_msgs::Point &p){
+    //implement here
+}
+
+void ProfileManagerRos::run(){
+    //implement here
+}
+
+void ProfileManagerRos::callDinsowConfig(){
+    //implement here
+}
