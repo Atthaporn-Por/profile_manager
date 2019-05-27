ad9d0e7fea5d9ac5c3da52793a0a7ca18fdc2d4a
Atthapron Phinitnart <qwertygms@hotmail.com>
2019-05-27 10:32:30 +0700

    add state, config, profiles and strategy in ProfileManager

--- include/profile_manager/profile_manager.h
+++ include/profile_manager/profile_manager.h
@@ -5,12 +5,21 @@
 
 #include <boost/thread.hpp>
 #include <profile_manager/config.h>
+#include <profile_manager/state.h>
+#include <profile_manager/manager_strategy.h>
+#include <profile_manager/profile.h>
 
 class ProfileManager{
     protected:
         bool starting;
         bool isReady;
 
+        State *state;
+        Config config;
+        Profiles profiles;
+
+        ManagerStrategy manager_strategy;
+        
         boost::thread worker;
         boost::mutex mutex;
         boost::condition_variable cv;
