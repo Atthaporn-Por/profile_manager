fc62f7bc5fd7ef792900df5da66aa7d2689fc6bb
Atthapron Phinitnart <qwertygms@hotmail.com>
2019-05-27 15:10:41 +0700

    edit state and thier child class

--- include/profile_manager/state.h
+++ include/profile_manager/state.h
@@ -2,69 +2,68 @@
 #define STATE_H
 
 #include <ros/ros.h>
+#include <profile_manager/constant.h>
+#include <profile_manager/manager_strategy.h>
 
 class State{
-    private:
+    protected:
         ros::Time before;
         ros::Time latest;
-        ros::Time timeout;
+        ros::Duration timeout;
         bool first_call;
     public:
         State();
         State(double timeout);
-        virtual void execute();
-        virtual std::string getStateName();
+        virtual void execute(ManagerStrategy *manager);
+        virtual int getStateName();
 };
 
 class IdleState : public State{
     public:
         IdleState();
         IdleState(double timeout);
-        void execute();
-        std::string getStateName();
+        void execute(ManagerStrategy *manager);
+        int getStateName();
 };
 
 class FirstState : public State{
     public:
         FirstState();
         FirstState(double timeout);
-        void execute();
-        std::string getStateName();
+        void execute(ManagerStrategy *manager);
+        int getStateName();
 };
 
 class SecondState : public State{
     public:
         SecondState();
         SecondState(double timeout);
-        void execute();
-        std::string getStateName();
+        void execute(ManagerStrategy *manager);
+        int getStateName();
 };
 
 class ThirdState : public State{
     public:
         ThirdState();
         ThirdState(double timeout);
-        void execute();
-        std::string getStateName();
+        void execute(ManagerStrategy *manager);
+        int getStateName();
 };
 
 class FourthState : public State{
     public:
         FourthState();
         FourthState(double timeout);
-        void execute();
-        std::string getStateName();
+        void execute(ManagerStrategy *manager);
+        int getStateName();
 };
 
 class EndState : public State{
     public:
         EndState();
         EndState(double timeout);
-        void execute();
-        std::string getStateName();
+        void execute(ManagerStrategy *manager);
+        int getStateName();
 };
-
-
-
 
 #endif
--- src/profile_manager/state.cpp
+++ src/profile_manager/state.cpp
@@ -1,69 +1,123 @@
 #include <profile_manager/state.h>
 
 State::State(){
-    //implement here
+    this->before = ros::Time::now();
+    this->latest = ros::Time::now();
+    this->timeout = ros::Duration(2.0);
+    this->first_call = false;
 }
+
+State::State(double timeout){
+    this->before = ros::Time::now();
+    this->latest = ros::Time::now();
+    this->timeout = ros::Duration(timeout);
+    this->first_call = false;
+}
+/*---------------------------- IdleState ---------------------------*/
+IdleState::IdleState() : State() {
+    first_call = true;
+}
+
+IdleState::IdleState(double timeout) : State(timeout) {
+    first_call = true;
+}
+
+void IdleState::execute(ManagerStrategy *manager){
+    //implement here
+    if(first_call){
+        if(ros::Time::now() - latest > ros::Duration(0.5)){
+            this->first_call = false;
+        }else if(ros::Time::now() - latest > timeout){
+            this->first_call = false;
+        }
+    }
+}
+
+int IdleState::getStateName(){
+    return IDLE_STATE;
+}
+
 /*---------------------------- FirstState ---------------------------*/
-FirstState::FirstState(){
+FirstState::FirstState() : State(){
+    first_call = true;
+}
+
+FirstState::FirstState(double timeout) : State(timeout){
+    first_call = true;
+}
+
+void FirstState::execute(ManagerStrategy *manager){
     //implement here
 }
 
-void FirstState::execute(){
-    //implement here
-}
-
-std::string FirstState::getStateName(){
-    //implement here
+int FirstState::getStateName(){
+    return STATE_1;
 }
 
 /*---------------------------- SecondState ---------------------------*/
-SecondState::SecondState(){
-    //implement here
+SecondState::SecondState() : State() {
+    first_call = true;
 }
 
-void SecondState::execute(){
+SecondState::SecondState(double timeout) : State(timeout) {
+    first_call = true;
+}
+
+void SecondState::execute(ManagerStrategy *manager){
     //implement here
 }   
 
-std::string SecondState::getStateName(){
-    //implement here
+int SecondState::getStateName(){
+    return STATE_2;
 }
 
 /*---------------------------- ThirdState ---------------------------*/
-ThirdState::ThirdState(){
+ThirdState::ThirdState() : State() {
+    first_call = true;
+}
+
+ThirdState::ThirdState(double timeout) : State(timeout) {
+    first_call = true;
+} 
+
+void ThirdState::execute(ManagerStrategy *manager){
     //implement here
 }
 
-void ThirdState::execute(){
-    //implement here
-}
-
-std::string ThirdState::getStateName(){
-    //implement here
+int ThirdState::getStateName(){
+    return STATE_3;
 }
 
 /*---------------------------- FourthState ---------------------------*/
-FourthState::FourthState(){
+FourthState::FourthState() : State() {
+    first_call = true;
+}
+
+FourthState::FourthState(double timeout) : State(timeout) {
+    first_call = true;
+}
+
+void FourthState::execute(ManagerStrategy *manager){
     //implement here
 }
 
-void FourthState::execute(){
-    //implement here
-}
-
-std::string FourthState::getStateName(){
-    //implement here
+int FourthState::getStateName(){
+    return STATE_4;
 }
 
 /*---------------------------- EndState ---------------------------*/
-EndState::EndState(){
+EndState::EndState() : State() {
+    first_call = true;
+}
+
+EndState::EndState(double timeout) : State(timeout) {
+    first_call = true;
+} 
+
+void EndState::execute(ManagerStrategy *manager){
     //implement here
 }
 
-void EndState::execute(){
-    //implement here
-}
-
-std::string EndState::getStateName(){
-    //implement here
+int EndState::getStateName(){
+    return END_STATE;
 }
